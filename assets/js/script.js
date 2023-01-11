3ceab020d98627fd3c3896097b41d7f9dd50e241
Tristan Viernes <trztn.keith@gmail.com>
2023-01-11 18:22:24 +0800

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function moveEmojiLeft(){
 }
 
 function moveEmojiRight(){
-    if (emojiLeft <= 340) {
+    if (emojiLeft <= 300) {
         emojiLeft += 70;
         emoji.style.left = emojiLeft + 'px';
     }
@@ -37,13 +37,13 @@ function displayScore() {
 }
 
 function generateShapes() {
-    let shapeBottom = 670;
-    let shapeLeft = Math.floor(Math.random() * 340);
+    let shapeBottom = 650;
+    let shapeLeft = Math.floor(Math.random() * 320);
     let shape = document.createElement('div');
     shape.setAttribute('class', `shape${Math.floor(Math.random() * 12)}`)
     shapes.appendChild(shape);
     function fallDownShape(){
-        if(shapeBottom < emojiBottom + 50 && shapeBottom > emojiBottom && shapeLeft > emojiLeft - 30 && shapeLeft < emojiLeft + 80) {
+        if(shapeBottom < emojiBottom + 40 && shapeBottom > emojiBottom && shapeLeft > emojiLeft - 40 && shapeLeft < emojiLeft + 80) {
             shapes.removeChild(shape);
             clearInterval(fallInterval);
             score += 10;
@@ -56,15 +56,16 @@ function generateShapes() {
             document.getElementById("buttonLeft").style.display = 'none';
             document.getElementById("buttonRight").style.display = 'none';
             clearInterval(fallInterval);
-            clearInterval(shapeTimeout); 
+            clearInterval(shapeTimeout);
         }
         shapeBottom -= 7;
         shape.style.bottom = shapeBottom + 'px';
         shape.style.left = shapeLeft + 'px';
     }
     let fallInterval = setInterval(fallDownShape, 20);
-    var shapeTimeout = setTimeout(generateShapes, 2000);
+    var shapeTimeout = setTimeout(generateShapes, 1500);
 }
+
 function replayGame(){
 location.reload();
 }
