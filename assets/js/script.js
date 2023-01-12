ff485fa6affd8ae98679578b48bcb03f882e9dd8
Tristan Viernes <trztn.keith@gmail.com>
2023-01-12 08:53:52 +0800

    fix minor issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ document.getElementById("buttonLeft").style.display = 'block';
 document.getElementById("buttonRight").style.display = 'block';
 document.getElementById("alertBox").style.display = "none";
 let score = 0;
+let lnum = 0
 
 function moveEmojiLeft(){
     if (emojiLeft >= 0) {
@@ -37,33 +38,38 @@ function displayScore() {
 }
 
 function generateShapes() {
-    let shapeBottom = 650;
-    let shapeLeft = Math.floor(Math.random() * 320);
-    let shape = document.createElement('div');
-    shape.setAttribute('class', `shape${Math.floor(Math.random() * 12)}`)
-    shapes.appendChild(shape);
-    function fallDownShape(){
-        if(shapeBottom < emojiBottom + 40 && shapeBottom > emojiBottom && shapeLeft > emojiLeft - 40 && shapeLeft < emojiLeft + 80) {
-            shapes.removeChild(shape);
-            clearInterval(fallInterval);
-            score += 10;
-            displayScore();
+    if(lnum < 1) {
+        let shapeBottom = 600;
+        let shapeLeft = Math.floor(Math.random() * 320);
+        let shape = document.createElement('div');
+        shape.setAttribute('class', `shape${Math.floor(Math.random() * 12)}`)
+        shapes.appendChild(shape);
+        function fallDownShape(){
+            if(shapeBottom < emojiBottom + 40 && shapeBottom > emojiBottom && shapeLeft > emojiLeft - 40 && shapeLeft < emojiLeft + 80) {
+                shapes.removeChild(shape);
+                clearInterval(fallInterval);
+                score += 10;
+                displayScore();
+            }
+            if(shapeBottom < emojiBottom){
+                document.getElementById("alertBox").style.display = 'block';
+                document.getElementById("gameOver").innerHTML = `Game Over!`;
+                document.getElementById("gameMessage").innerHTML = `Contents Here!`;
+                document.getElementById("buttonLeft").style.display = 'none';
+                document.getElementById("buttonRight").style.display = 'none';
+                clearInterval(fallInterval);
+                clearInterval(shapeTimeout);
+                shapes.remove();
+                emoji.remove();
+                ++lnum;
+            }
+            shapeBottom -= 7;
+            shape.style.bottom = shapeBottom + 'px';
+            shape.style.left = shapeLeft + 'px';
         }
-        if(shapeBottom < emojiBottom){
-            document.getElementById("alertBox").style.display = 'block';
-            document.getElementById("gameOver").innerHTML = `Game Over!`;
-            document.getElementById("gameMessage").innerHTML = `Contents Here!`;
-            document.getElementById("buttonLeft").style.display = 'none';
-            document.getElementById("buttonRight").style.display = 'none';
-            clearInterval(fallInterval);
-            clearInterval(shapeTimeout);
-        }
-        shapeBottom -= 7;
-        shape.style.bottom = shapeBottom + 'px';
-        shape.style.left = shapeLeft + 'px';
+        let fallInterval = setInterval(fallDownShape, 20);
+        var shapeTimeout = setTimeout(generateShapes, 1500);
     }
-    let fallInterval = setInterval(fallDownShape, 20);
-    var shapeTimeout = setTimeout(generateShapes, 1500);
 }
 
 function replayGame(){
